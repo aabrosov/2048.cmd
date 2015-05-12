d9cd74c001a8d6a9a14e3b5bdd0995a6af57df7f
aabrosov <aabrosov@rs.com>
2015-05-12 11:01:13 +0500

    added alternative keys and check before exit

--- 2048.cmd
+++ 2048.cmd
@@ -29,27 +29,45 @@ echo score = !score!
 echo bests = !bests!
 echo.
 rem choice /c xwasdr /n /m " [w]  [a]  [s]  [d] or e[x]it or [r]eset"
-echo. [w]  [a]  [s]  [d] or e[x]it or [r]eset
+echo. [w^|i^|8]  [a^|j^|4]  [s^|k^|2]  [d^|l^|6] or e[x]it or [q]uit or [r]eset
+:getkey
 set key=
 for /f "delims=" %%a in ('xcopy /w "%~f0" "%~f0" 2^>nul') do if not defined key set key=%%a
 set "key=%key:~-1%"
 if /i "%key%"=="r" goto :init
-if /i "%key%"=="d" (
-	for %%i in (1 2 3 4) do call :calc a%%i1 a%%i2 a%%i3 a%%i4
-	goto :main
-)
-if /i "%key%"=="s" (
-	for %%i in (1 2 3 4) do call :calc a1%%i a2%%i a3%%i a4%%i
-	goto :main
-)
-if /i "%key%"=="a" (
-	for %%i in (1 2 3 4) do call :calc a%%i4 a%%i3 a%%i2 a%%i1
-	goto :main
-)
-if /i "%key%"=="w" (
-	for %%i in (1 2 3 4) do call :calc a4%%i a3%%i a2%%i a1%%i
-	goto :main
-)
+if /i "%key%"=="d" goto :right
+if /i "%key%"=="l" goto :right
+if /i "%key%"=="6" goto :right
+if /i "%key%"=="s" goto :down
+if /i "%key%"=="k" goto :down
+if /i "%key%"=="2" goto :down
+if /i "%key%"=="a" goto :left
+if /i "%key%"=="j" goto :left
+if /i "%key%"=="4" goto :left
+if /i "%key%"=="w" goto :up
+if /i "%key%"=="i" goto :up
+if /i "%key%"=="8" goto :up
+if /i "%key%"=="x" goto :exit
+if /i "%key%"=="q" goto :exit
+goto :getkey
+:up
+for %%i in (1 2 3 4) do call :calc a4%%i a3%%i a2%%i a1%%i
+goto :main
+:down
+for %%i in (1 2 3 4) do call :calc a1%%i a2%%i a3%%i a4%%i
+goto :main
+:left
+for %%i in (1 2 3 4) do call :calc a%%i4 a%%i3 a%%i2 a%%i1
+goto :main
+:right
+for %%i in (1 2 3 4) do call :calc a%%i1 a%%i2 a%%i3 a%%i4
+goto :main
+:exit
+echo.are you sure? [y/n]
+set key=
+for /f "delims=" %%a in ('xcopy /w "%~f0" "%~f0" 2^>nul') do if not defined key set key=%%a
+set "key=%key:~-1%"
+if /i "%key%" neq "y" goto :main
 if !score! gtr !bests! set bests=!score!
 echo !bests!>score_2048.txt
 goto :eof
